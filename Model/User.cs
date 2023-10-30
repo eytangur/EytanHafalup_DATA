071148c36b8b7d029b1eb2d9061b81a25295d7a1
User <User@WXI1124925>
2023-10-30 16:23:25 +0200

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User : BaseEntity
+    {
+        protected string username;// שם משתמש
+        protected string password;//סיסמה
+        protected bool gender;//מין
+        protected DateTime bDay;//תאריך לידה
+        protected int phoneNum;//מספר טלפון
+        protected bool isManager;//?האם מנהל
+        protected string email;//חחשבון אימייל
+        public string userName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Password 
+        {
+            get { return password; }
+            set { password = value; }
+        }      
+        public DateTime BDay
+        {
+            get { return bDay; }
+            set { bDay = value; }
+        }       
+        public bool Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }       
+        public int PhoneNum
+        {
+            get { return phoneNum; }
+            set { phoneNum = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public bool IsManager
+        {
+            get { return isManager; }
+            set { isManager = value; }
+        }
+    }
+    public class UserList : List<User>
+    {
+        //בנאי ברירת מחדל - אוסף ריק
+        public UserList() { }
+        //המרה אוסף גנרי לרשימת משתמשים
+        public UserList(IEnumerable<User> list)
+            : base(list) { }
+        //המרה מטה מטיפוס בסיס לרשימת משתמשים
+        public UserList(IEnumerable<BaseEntity> list)
+            : base(list.Cast<User>().ToList()) { }
+    }
+}
---
+++ ViewModel/UserDB.cs
@@ -0,0 +1,45 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.Id = int.Parse(reader["id"].ToString());
+            user.Email = reader["email"].ToString();
+            user.Password = reader["password"].ToString();
+            user.PhoneNum = int.Parse(reader["phoneNum"].ToString());
+            user.userName = reader["username"].ToString();
+            user.Gender = bool.Parse(reader["gender"].ToString());
+            user.BDay = DateTime.Parse(reader["bDay"].ToString());
+            user.IsManager = bool.Parse(reader["isManager"].ToString());
+            return user;
+        }
+
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tbCity";
+            UserList list = new UserList(ExecuteCommand());
+            return list;
+        }
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tbCity WHERE id=" + id;
+            UserList list = new UserList(ExecuteCommand());
+            if (list.Count == 0)
+                return null;
+            return list[0];
+        }
+    }
+}
